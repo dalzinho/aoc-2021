97995154890007a9075bd0757e5e84b53aad85ae
jpdal <jpdaly@gmail.com>
2021-12-07 09:06:24 +0000

    adds day 6 ,7 solutions

---
+++ src/main/java/uk/co/mrdaly/adventofcode2021/_07TheTreacheryOfWhales/TheTreacheryOfWhales.java
@@ -0,0 +1,49 @@
+package uk.co.mrdaly.adventofcode2021._07TheTreacheryOfWhales;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.LongUnaryOperator;
+
+public class TheTreacheryOfWhales {
+
+    public long partOne(List<Integer> input) {
+        return calculateFuel(input, calculateMedian(input), l -> l);
+    }
+
+    private long calculateFuel(List<Integer> input, long stat, LongUnaryOperator fuelFactor) {
+        return input.stream()
+                .mapToLong(i -> Math.abs(i - stat))
+                .filter(l -> l != 0)
+                .map(fuelFactor)
+                .sum();
+    }
+
+    private int calculateMedian(List<Integer> ints) {
+        Collections.sort(ints);
+        int median;
+        int middle = ints.size() / 2;
+
+        if (ints.size() % 2 == 0) {
+            median = (ints.get(middle - 1) + ints.get(middle)) / 2;
+        } else {
+            median = ints.get(middle);
+        }
+        return median;
+    }
+
+    public long partTwo(List<Integer> input) {
+        double mean = input.stream().mapToInt(Integer::intValue).average().orElse(0);
+
+        double floor = calculateFuel(input, (long) Math.floor(mean), this::calc);
+        double ceil = calculateFuel(input, (long) Math.ceil(mean), this::calc);
+        return (long) Math.min(floor, ceil);
+    }
+
+    private long calc(long l) {
+        if (l == 1) {
+            return 1;
+        }
+
+        return l + calc(l - 1);
+    }
+}
---
+++ src/test/java/uk/co/mrdaly/adventofcode2021/_07TheTreacheryOfWhales/TheTreacheryOfWhalesTest.java
@@ -0,0 +1,60 @@
+package uk.co.mrdaly.adventofcode2021._07TheTreacheryOfWhales;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.mrdaly.adventofcode2021.util.StringFileReader;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class TheTreacheryOfWhalesTest {
+
+    private TheTreacheryOfWhales theTreacheryOfWhales;
+    private List<Integer> sampleInput;
+    private List<Integer> actualInput;
+
+    @Before
+    public void setup() {
+        theTreacheryOfWhales = new TheTreacheryOfWhales();
+        sampleInput = parseInput("16,1,2,0,4,2,7,1,2,14");
+        actualInput = StringFileReader.getInstance("_07TheTreacheryOfWhales/07input1.txt").read()
+                .map(this::parseInput)
+                .findFirst()
+                .get();
+    }
+
+    private List<Integer> parseInput(String input) {
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    public void partOne_sampleData() {
+        long actual = theTreacheryOfWhales.partOne(sampleInput);
+        assertEquals(37, actual);
+    }
+
+    @Test
+    public void partOne() {
+        long actual = theTreacheryOfWhales.partOne(actualInput);
+        assertEquals(328262, actual);
+    }
+
+    @Test
+    public void partTwo_sampleData() {
+        long actual = theTreacheryOfWhales.partTwo(sampleInput);
+        assertEquals(168, actual);
+    }
+
+    @Test
+    public void partTwo() {
+        long actual = theTreacheryOfWhales.partTwo(actualInput);
+        assertEquals(90040997, actual);
+    }
+
+
+}
